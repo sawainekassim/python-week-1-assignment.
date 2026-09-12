3e4feb564c55f48a375a2b7b34d267fcd5186524
sawainekassim <sawainekassim@gmail.com>
2026-09-12 18:03:13 +0300

    Introduce self and course goals in week1_assignment.py

--- week1_assignment.py
+++ week1_assignment.py
@@ -1,1 +1,11 @@
+# This program introduces me and my goal for the course.
 
+name = "Alex"
+age = 22
+favourite_language = "Python"
+goal = "to learn programming and build useful projects"
+
+print("Hello! My name is", name + ".")
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language + ".")
+print("My goal for this course is", goal + ".")
